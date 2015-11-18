c2d16abfbe8153a291fa027b5caff55a6082305f
Yue Ko <yko@infoblox.com>
2015-11-18 11:18:03 -0500

    Added utility to determine supported feature
    Added Feature class that is used to determine Infoblox features
    supported by a WAPI version.

--- infoblox_client/connector.py
+++ infoblox_client/connector.py
@@ -13,7 +13,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from __future__ import absolute_import
 import functools
 import re
 import requests
