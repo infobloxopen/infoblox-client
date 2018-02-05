c70c5105644077da6df97a09c5419fa45173504e
Akash Deep <adsri>
2018-02-05 10:43:36 +0100

    changed LOG Error to Warning when object search fails
    Treating search failure as warning and returning None in infoblox client,
    as plugin using the infoblox_client can LOG based on its requirement.

--- infoblox_client/connector.py
+++ infoblox_client/connector.py
@@ -319,7 +319,7 @@ class Connector(object):
         self._validate_authorized(r)
 
         if r.status_code != requests.codes.ok:
-            LOG.error("Error occurred on object search: %s", r.content)
+            LOG.warning("Failed on object search with url %s: %s", url, r.content)
             return None
         return self._parse_reply(r)
 
