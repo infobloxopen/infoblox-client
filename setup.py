c40b562b4a02128001bbc818a012993cd824b4b5
Exane Server Team <equipe_serveurs@exane.com>
2018-11-05 17:53:07 +0100

    Fix test_suite location. (#205)
    Makes ``make test`` & coverage work again

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-    test_suite='infoblox_client.tests',
+    test_suite='tests',
     tests_require=testing_requirements
 )
