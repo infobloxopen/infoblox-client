91b870e47bd96a9d4ea177fa891e88f0dc87b3eb
Rajath Av <54664782+AvRajath@users.noreply.github.com>
2020-03-12 16:45:40 +0530

    Bump up version to 0.4.25 (#251)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open('testing_requirements.txt') as requirements_file:
 
 setup(
     name='infoblox-client',
-    version='0.4.24',
+    version='0.4.25',
     description="Client for interacting with Infoblox NIOS over WAPI",
     long_description=readme + '\n\n' + history,
     long_description_content_type='text/markdown',
