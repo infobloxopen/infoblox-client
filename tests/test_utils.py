fc793bcfc3d24ff0297a1b388ad029de245a5553
Alex Chernevskiy <achernevskii@infoblox.com>
2022-06-15 14:40:00 -0500

    Added paging function in the infoblox_client.utils module, updated doc
    * Added paging function in the infoblox_client.utils module. This function
      splits WAPI response by pages.
    
    * Added an example on how to implement page by page output using the
      paging function.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -90,3 +90,13 @@ class TestUtils(unittest.TestCase):
         mac = 123
         with self.assertRaises(ValueError):
             utils.generate_duid(mac)
+
+    def test_paging(self):
+        resp = [1, 2, 3, 4]
+        i = 0
+        max_results = 2
+        for page in utils.paging(resp, max_results=max_results):
+            self.assertEqual(2, len(page))
+            self.assertEqual(resp[i:i+max_results], page)
+            i = i + max_results
+
