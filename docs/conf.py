beaf243892576da55c250f95618e91aeb6e8f21b
Alex Chernevskiy <achernevskii@infoblox.com>
2022-07-21 16:35:49 -0500

    Added auto-generated rst pages to Sphinx exclude_patterns

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,7 @@ release = infoblox_client.__version__
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'modules.rst', 'infoblox_client.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
